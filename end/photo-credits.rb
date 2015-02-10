652b7e729a94382dab6d551faadfb6a394a6e17a
Ben Crowell <notreal@fakewillnotwork.invalid>
2015-02-09 21:30:43 -0800

    ladybug example

--- end/photo-credits.rb
+++ end/photo-credits.rb
@@ -19,7 +19,7 @@ photo_credit("twin-paradox","Rotating earth","NASA image, public domain; animate
   # http://commons.wikimedia.org/wiki/File:Rotating_earth_%28large%29.gif
 photo_credit("cern-muons","Muon storage ring at CERN","(c) 1974 by CERN; used here under the U.S.~fair use doctrine")
 photo_credit("galilean-transformation","Cow and car","As for the figure on p.~\\pageref{fig:cow-and-car}")
-photo_credit("galaxy-lorentz-contraction","Galaxy","NASA/STScI/ESA, public domain")
+photo_credit("galaxy-length-contraction","Galaxy","NASA/STScI/ESA, public domain")
   # http://upload.wikimedia.org/wikipedia/commons/b/b3/UGC_12158.jpg
 photo_credit("convertible-and-cube","Cadillac","Redrawn from a photo by That Hartford Guy, CC-BY-SA")
   # http://commons.wikimedia.org/wiki/File:1965_Cadillac_Deville_convertible_left.jpg
